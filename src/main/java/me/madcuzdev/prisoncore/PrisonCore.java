d50fb4658bfbc456191cdab5780509c4f921199c
MadCuzDev <devan200222@gmail.com>
2019-01-31 21:01:07 -0500

    initial commit

---
+++ src/main/java/me/madcuzdev/prisoncore/PrisonCore.java
@@ -0,0 +1,17 @@
+package me.madcuzdev.prisoncore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PrisonCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
